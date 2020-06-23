753b93aab4da2139068eae2ed7ae7b797e3f6981
PC Casa <thiago.infortec@gmail.com>
2020-06-23 00:45:05 -0300

    Beging Front

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+import api from './services/api'
+
 import Header from './components/Header';
 
+
+import './App.css'
+
 function App(){
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data); 
+    })
+  }, [])
 
   function handleAddProject() {
     //projects.push(`Novo Projeto ${Date.now()}`);
@@ -15,7 +26,7 @@ function App(){
     <Header title="Projects"/>
 
     <ul>
-      {projects.map(project => <li key={project}>{project}</li>)}
+      {projects.map(project => <li key={project.id}>{project.title}</li>)}
     </ul>
 
     <button type="button" onClick={handleAddProject}> Adicionar projeto</button>
